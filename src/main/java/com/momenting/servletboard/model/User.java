16be9d9d73df79bd8679b0ff431634f684d07dd9
kangyoung558 <58406058+kangyoung558@users.noreply.github.com>
2021-11-03 11:11:41 +0900

    1. 환경설정 완료

---
+++ src/main/java/com/momenting/servletboard/model/User.java
@@ -0,0 +1,11 @@
+package com.momenting.servletboard.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class User {
+	private String name;
+	private String pwd;
+}
